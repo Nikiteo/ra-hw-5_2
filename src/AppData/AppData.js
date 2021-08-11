23a1fdab5392da414baa6dcb3f933feb872b9380
Nikiteo <zhigiley.nikita2012@yandex.ru>
2021-08-11 15:59:44 +0300

    Initial

---
+++ src/AppData/AppData.js
@@ -0,0 +1,131 @@
+import logo from './logo.png';
+import cloud from './cloud.png';
+
+export const news = {
+  "uniqId": "news",
+  "interval": 8000,
+  "firstInterval": 20000,
+  "favoriteIcon": "dot",
+  "showIcons": true,
+  "newTabLinks": true,
+  "data": {
+    "region": {
+      "showIcons": true,
+      "newTabLinks": true,
+      "statid": "news.region",
+      "name": "region",
+      "news": [{
+        "full": "В минэкологии рассказали, как вести себя при встрече с дикими животными",
+        "ishot": null,
+        "href": "https://yandex.ru/news/story/Vminehkologii_rasskazali_kak_vesti_sebya_privstreche_sdikimi_zhivotnymi--c4a1acc26dba7983aa71be80b55dc968?lang=ru&from=reg_portal&fan=1&stid=l-Z8V6KM2huRp8GZ&t=1628668177&persistent_id=155436199&lr=10748&msid=1628668571.65617.94825.136314&mlid=1628668177.geo_1.c4a1acc2&utm_source=morda_desktop&utm_medium=topnews_region",
+        "agency_logo": "https://avatars.mds.yandex.net/get-ynews-logo/786982/254151524-1528881720128-square/logo-square",
+        "agency_name": "ИА Красная Весна"
+      }, {
+        "full": "В Московской области зафиксировано 1 309 случаев COVID-19",
+        "ishot": null,
+        "href": "https://yandex.ru/news/story/VMoskovskoj_oblasti_zafiksirovano_1309_sluchaev_COVID-19--1069e8709f82d4774fc7e5a6b8b8735c?lang=ru&from=reg_portal&fan=1&stid=nsx86jfG8nPF-JhZOYnA&t=1628668177&persistent_id=146548156&lr=10748&msid=1628668571.65617.94825.136314&mlid=1628668177.geo_1.1069e870&utm_source=morda_desktop&utm_medium=topnews_region",
+        "agency_logo": "https://avatars.mds.yandex.net/get-ynews-logo/135513/254154655-1516194420610-square/logo-square",
+        "agency_name": "Вести Подмосковья"
+      }, {
+        "full": "Жителей Подмосковья оштрафовали на 31,5 тысячи рублей за нарушения правил выгула собак",
+        "ishot": null,
+        "href": "https://yandex.ru/news/story/ZHitelej_Podmoskovya_oshtrafovali_na315_tysyachi_rublej_zanarusheniya_pravil_vygula_sobak--9271aac4e95925c768ec67802c380ff2?lang=ru&from=reg_portal&fan=1&stid=eaRGiByx4QGtNEx3c6lL&t=1628668177&persistent_id=155404936&lr=10748&msid=1628668571.65617.94825.136314&mlid=1628668177.geo_1.9271aac4&utm_source=morda_desktop&utm_medium=topnews_region",
+        "agency_logo": "https://avatars.mds.yandex.net/get-ynews-logo/62808/254061710-1496559028899-square/logo-square",
+        "agency_name": "Подмосковье Сегодня"
+      }, {
+        "full": "В Московской области благоустроили первые протоптанные дорожки",
+        "ishot": null,
+        "href": "https://yandex.ru/news/story/VMoskovskoj_oblasti_blagoustroili_pervye_protoptannye_dorozhki--de8a38b12b42a7cf2d912f42e6f50129?lang=ru&from=reg_portal&fan=1&stid=3ywkx0_wqrK3V6qCGwoq&t=1628668177&persistent_id=155406251&lr=10748&msid=1628668571.65617.94825.136314&mlid=1628668177.geo_1.de8a38b1&utm_source=morda_desktop&utm_medium=topnews_region",
+        "agency_logo": "https://avatars.mds.yandex.net/get-ynews-logo/135513/254154655-1516194420610-square/logo-square",
+        "agency_name": "Вести Подмосковья"
+      }, {
+        "full": "Почти 1,1 тысячи семей Подмосковья бесплатно получили землю с января",
+        "ishot": null,
+        "href": "https://yandex.ru/news/story/Pochti_11_tysyachi_semej_Podmoskovya_besplatno_poluchili_zemlyu_syanvarya--aa541255eea53d5782885f5aa08ea133?lang=ru&from=reg_portal&fan=1&stid=p9IkUd18COTJGyBdM9-1&t=1628668177&persistent_id=155409548&lr=10748&msid=1628668571.65617.94825.136314&mlid=1628668177.geo_1.aa541255&utm_source=morda_desktop&utm_medium=topnews_region",
+        "agency_logo": "https://avatars.mds.yandex.net/get-ynews-logo/41096/11930-1522762922262-square/logo-square",
+        "agency_name": "Телеканал 360°"
+      }, ]
+    }
+  },
+  "header": [{
+    "title": "Сейчас в СМИ",
+    "link": "https://yandex.ru/news?msid=1628672871.06826.97919.133901&amp;mlid=1628672381.glob_225&amp;utm_source=morda_desktop&amp;utm_medium=topnews_news",
+  }, {
+    "title": "Москва и область",
+    "link": "https://yandex.ru/news/region/moscow_and_moscow_oblast?msid=1628668571.65617.94825.136314&mlid=1628668177.geo_1&utm_source=morda_desktop&utm_medium=topnews_region",
+  }, {
+    "title": "Интересное",
+    "link": "https://yandex.ru/news/rubric/personal_feed?utm_source=morda_desktop&utm_medium=topnews_personal",
+  }],
+};
+
+export const stocks = [{
+    "title": "USD",
+    "value": "74,04",
+    "value_change": "+0,11",
+  }, {
+    "title": "EUR",
+    "value": "86,66",
+    "value_change": "+0,02",
+  },
+  {
+    "title": "Нефть",
+    "value": "70,56",
+    "value_change": "−0,38%",
+  }
+];
+
+export const search = {
+  "menu": [
+    "Видео",
+    "Картинки",
+    "Новости",
+    "Карты",
+    "Маркет",
+    "Переводчик",
+    "Эфир",
+    "еще",
+  ],
+  logo,
+  "searchIdea": "потерявшиеся ключи",
+};
+
+export const banner = {
+  "src": "https://placehold.it/918x114",
+  "alt": "Banner"
+};
+
+export const weather = {
+  icon: cloud,
+  temp: '+17',
+  morning: '+17',
+  day: '+20',
+};
+
+export const broadcast = [{
+    text: 'Хрень по ящику',
+    channel: 'ХрТв',
+  },
+  {
+    text: "Ешь мед как Винни О'Пух",
+    channel: 'Зомби-ТВ',
+  },
+  {
+    text: 'Топ Гиря. Ускорение',
+    channel: 'Бритиш-Онлайн',
+  },
+];
+
+export const mostVisited = [{
+    category: 'Недвижимость',
+    description: 'Хруща или замкад?',
+  },
+  {
+    category: 'Маркет',
+    description: 'Телеки размером с окно',
+  },
+  {
+    category: 'Авто',
+    description: 'Ищу за 150, шлеп!',
+  },
+];
